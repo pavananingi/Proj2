4a6228a8766a95307d0ec78279dc372501342527
pavananingi <pavan.aningi1998@gmail.com>
2025-07-04 10:16:07 +0530

    good

---
+++ postcss.config.js
@@ -0,0 +1,15 @@
+// export default {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// }
+
+
+// postcss.config.js
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
